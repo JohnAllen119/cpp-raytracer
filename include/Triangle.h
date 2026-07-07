f1b9877c0ca5966175df39f288d2d0dbfe0f44bd
JohnAllen119 <fong69173@gmail.com>
2026-07-07 16:38:58 +0800

    support obj vertex normals for smooth shading

--- include/Triangle.h
+++ include/Triangle.h
@@ -5,13 +5,28 @@ class Triangle : public Object {
 public:
   Vec3 v0, v1, v2;
   Vec2 uv0, uv1, uv2;
+  Vec3 n0, n1, n2;
+  bool has_vertex_normals;
   Triangle(Vec3 v0, Vec3 v1, Vec3 v2, Material *mat)
-      : v0(v0), v1(v1), v2(v2), Object(mat), uv0(0, 0), uv1(0, 0), uv2(0, 0) {}
+      : v0(v0), v1(v1), v2(v2), Object(mat), uv0(0, 0), uv1(0, 0), uv2(0, 0),
+        n0(Vec3(0, 0, 0)), n1(Vec3(0, 0, 0)), n2(Vec3(0, 0, 0)),
+        has_vertex_normals(false) {}
   Triangle(Vec3 v0, Vec3 v1, Vec3 v2)
-      : v0(v0), v1(v1), v2(v2), Object(nullptr), uv0(0, 0), uv1(0, 0), uv2(0, 0) {}
-   Triangle(Vec3 a, Vec3 b, Vec3 c, Vec2 uv0,Vec2 uv1,Vec2 uv2, Material *mat)
-      : v0(a), v1(b), v2(c), uv0(uv0),uv1(uv1),uv2(uv2),Object(mat) {}
-      bool is_hit(const Ray &ray, double t_min, double t_max,
+      : v0(v0), v1(v1), v2(v2), Object(nullptr), uv0(0, 0), uv1(0, 0),
+        uv2(0, 0), n0(Vec3(0, 0, 0)), n1(Vec3(0, 0, 0)), n2(Vec3(0, 0, 0)),
+        has_vertex_normals(false) {}
+  Triangle(Vec3 a, Vec3 b, Vec3 c, Vec2 uv0, Vec2 uv1, Vec2 uv2, Material *mat)
+      : v0(a), v1(b), v2(c), uv0(uv0), uv1(uv1), uv2(uv2), Object(mat),
+        n0(Vec3(0, 0, 0)), n1(Vec3(0, 0, 0)), n2(Vec3(0, 0, 0)),
+        has_vertex_normals(false) {}
+  Triangle(Vec3 a, Vec3 b, Vec3 c, Vec2 uv0, Vec2 uv1, Vec2 uv2, Vec3 n0,
+           Vec3 n1, Vec3 n2, Material *mat)
+      : v0(a), v1(b), v2(c), uv0(uv0), uv1(uv1), uv2(uv2), Object(mat), n0(n0),
+        n1(n1), n2(n2), has_vertex_normals(true) {}
+  Triangle(Vec3 a, Vec3 b, Vec3 c, Vec3 n0, Vec3 n1, Vec3 n2, Material *mat)
+      : v0(a), v1(b), v2(c), uv0(Vec2(0, 0)), uv1(Vec2(0, 0)), uv2(Vec2(0, 0)),
+        Object(mat), n0(n0), n1(n1), n2(n2), has_vertex_normals(true) {}
+  bool is_hit(const Ray &ray, double t_min, double t_max,
               hit_record &rec) const override {
     Vec3 E1 = v1 - v0;
     Vec3 E2 = v2 - v0;
@@ -40,44 +55,53 @@ public:
     rec.obj = const_cast<Triangle *>(this);
 
     rec.u = tex_u;
-    rec.v = tex_u;
+    rec.v = tex_v;
     return true;
   }
-  Vec3 getnormal(const Vec3 &Point)const override{
-    Vec3 E1=v1-v0;
-    Vec3 E2=v2-v0;
-    return E1.cross(E2).normalize();
-  }
-  AABB getbounding() const override{
-        Vec3 min(
-        std::min({v0.x, v1.x, v2.x}),
-        std::min({v0.y, v1.y, v2.y}),
-        std::min({v0.z, v1.z, v2.z})
-    );
-    Vec3 max(
-        std::max({v0.x, v1.x, v2.x}),
-        std::max({v0.y, v1.y, v2.y}),
-        std::max({v0.z, v1.z, v2.z})
-    );
-    return AABB(min, max);
+  Vec3 getnormal(const Vec3 &Point) const override {
+    Vec3 E1 = v1 - v0;
+    Vec3 E2 = v2 - v0;
+    Vec3 face_normal = E1.cross(E2).normalize();
+    if (!has_vertex_normals) {
+      return face_normal;
     }
-    void getuv(double &u, double &v, const Vec3 &hitPoint) const override{
-        //hitpoint_weight
-        Vec3 E1=v1-v0;
-        Vec3 E2=v2-v0;
-        Vec3 P=hitPoint-v0;
-        double d00=E1.dot(E1);
-        double d01=E1.dot(E2);
-        double d11=E2.dot(E2);
-        double d20=P.dot(E1);
-        double d21=P.dot(E2);
-        double denom=d00*d11-d01*d01;
-        double beta=(d11*d20-d01*d21)/denom;
-        double gamma=(d00*d21-d01*d20)/denom;
-        double alpha=1.0-beta-gamma;
+     Vec3 P = Point - v0;
+    double d00 = E1.dot(E1);
+    double d01 = E1.dot(E2);
+    double d11 = E2.dot(E2);
+    double d20 = P.dot(E1);
+    double d21 = P.dot(E2);
+    double denom = d00 * d11 - d01 * d01;
+    double beta = (d11 * d20 - d01 * d21) / denom;
+    double gamma = (d00 * d21 - d01 * d20) / denom;
+    double alpha = 1.0 - beta - gamma;
+    Vec3 smooth_normal = n0 * alpha + n1 * beta + n2 * gamma;
 
-        u=uv0.x*alpha+uv1.x*beta+uv2.x*gamma;
-        v=uv0.y*alpha+uv1.y*beta+uv2.y*gamma;
-        
-    }
+    return smooth_normal.normalize();
+  }
+  AABB getbounding() const override {
+    Vec3 min(std::min({v0.x, v1.x, v2.x}), std::min({v0.y, v1.y, v2.y}),
+             std::min({v0.z, v1.z, v2.z}));
+    Vec3 max(std::max({v0.x, v1.x, v2.x}), std::max({v0.y, v1.y, v2.y}),
+             std::max({v0.z, v1.z, v2.z}));
+    return AABB(min, max);
+  }
+  void getuv(double &u, double &v, const Vec3 &hitPoint) const override {
+    // hitpoint_weight
+    Vec3 E1 = v1 - v0;
+    Vec3 E2 = v2 - v0;
+    Vec3 P = hitPoint - v0;
+    double d00 = E1.dot(E1);
+    double d01 = E1.dot(E2);
+    double d11 = E2.dot(E2);
+    double d20 = P.dot(E1);
+    double d21 = P.dot(E2);
+    double denom = d00 * d11 - d01 * d01;
+    double beta = (d11 * d20 - d01 * d21) / denom;
+    double gamma = (d00 * d21 - d01 * d20) / denom;
+    double alpha = 1.0 - beta - gamma;
+
+    u = uv0.x * alpha + uv1.x * beta + uv2.x * gamma;
+    v = uv0.y * alpha + uv1.y * beta + uv2.y * gamma;
+  }
 };
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,65 +1,91 @@
-#include"../include/Mesh.h"
+#include "../include/Mesh.h"
 
-
-Mesh::Mesh(const OBJData& data) : Object(nullptr) {
-    for (const auto& face : data.faces) {
-        int i = face.indices[0].v_index;
-        int j = face.indices[1].v_index;
-        int k = face.indices[2].v_index;
-        triangles.emplace_back(data.vertices[i], data.vertices[j], data.vertices[k], nullptr);
-    }
+Mesh::Mesh(const OBJData &data) : Object(nullptr) {
+  for (const auto &face : data.faces) {
+    int i = face.indices[0].v_index;
+    int j = face.indices[1].v_index;
+    int k = face.indices[2].v_index;
+    triangles.emplace_back(data.vertices[i], data.vertices[j], data.vertices[k],
+                           nullptr);
+  }
 }
 
-Mesh::Mesh(const OBJData& data, Material* m) : Object(m) {
-    for (const auto& face : data.faces) {
-        int i = face.indices[0].v_index;
-        int j = face.indices[1].v_index;
-        int k = face.indices[2].v_index;
+Mesh::Mesh(const OBJData &data, Material *m) : Object(m) {
+  for (const auto &face : data.faces) {
+    int i = face.indices[0].v_index;
+    int j = face.indices[1].v_index;
+    int k = face.indices[2].v_index;
 
-        Vec3 p0=data.vertices[i];
-        Vec3 p1=data.vertices[j];
-        Vec3 p2=data.vertices[k];
+    Vec3 p0 = data.vertices[i];
+    Vec3 p1 = data.vertices[j];
+    Vec3 p2 = data.vertices[k];
 
-        int uv_i=face.indices[0].vt_index;
-        int uv_j=face.indices[1].vt_index;
-        int uv_k=face.indices[2].vt_index;
+    int uv_i = face.indices[0].vt_index;
+    int uv_j = face.indices[1].vt_index;
+    int uv_k = face.indices[2].vt_index;
 
-        bool has_uv=uv_i >= 0 && uv_j >= 0 && uv_k >= 0 &&
+    bool has_uv = uv_i >= 0 && uv_j >= 0 && uv_k >= 0 &&
                   uv_i < data.texcoords.size() &&
                   uv_j < data.texcoords.size() && uv_k < data.texcoords.size();
 
-        if(has_uv){
-            Vec2 uv0=data.texcoords[uv_i];
-            Vec2 uv1=data.texcoords[uv_j];
-            Vec2 uv2=data.texcoords[uv_k];
-            triangles.emplace_back(p0, p1, p2,uv0,uv1,uv2, m);
-        }else {
-      triangles.emplace_back(p0, p1, p2, m);
-        
+    int n_i = face.indices[0].vn_index;
+    int n_j = face.indices[1].vn_index;
+    int n_k = face.indices[2].vn_index;
+    bool has_normal = n_i >= 0 && n_j >= 0 && n_k >= 0 &&
+                      n_i < data.normals.size() && n_j < data.normals.size() &&
+                      n_k < data.normals.size();
+    if (has_uv && has_normal) {
+      Vec2 uv0 = data.texcoords[uv_i];
+      Vec2 uv1 = data.texcoords[uv_j];
+      Vec2 uv2 = data.texcoords[uv_k];
+
+      Vec3 n0 = data.normals[n_i].normalize();
+      Vec3 n1 = data.normals[n_j].normalize();
+      Vec3 n2 = data.normals[n_k].normalize();
+      triangles.emplace_back(p0, p1, p2, uv0, uv1, uv2, n0, n1, n2, m);
+    } else if (has_uv) {
+      Vec2 uv0 = data.texcoords[uv_i];
+      Vec2 uv1 = data.texcoords[uv_j];
+      Vec2 uv2 = data.texcoords[uv_k];
+      triangles.emplace_back(p0, p1, p2, uv0, uv1, uv2, m);
+    } else if (has_normal) {
+      Vec3 n0 = data.normals[n_i].normalize();
+      Vec3 n1 = data.normals[n_j].normalize();
+      Vec3 n2 = data.normals[n_k].normalize();
+
+         triangles.emplace_back(p0, p1, p2,n0, n1, n2, m);
+    }else{
+        triangles.emplace_back(p0, p1, p2, m);
     }
+  }
 }
-}
-Vec3 Mesh::getnormal(const Vec3& point) const {
-    if (triangles.empty()) return Vec3(0, 1, 0);
-    return triangles[0].getnormal(point);
+Vec3 Mesh::getnormal(const Vec3 &point) const {
+  if (triangles.empty())
+    return Vec3(0, 1, 0);
+  return triangles[0].getnormal(point);
 }
 
 AABB Mesh::getbounding() const {
-    if (triangles.empty()) return AABB(Vec3(0, 0, 0), Vec3(0, 0, 0));
-    
-    Vec3 min = triangles[0].getbounding().min_bound;
-    Vec3 max = triangles[0].getbounding().max_bound;
-    
-    for (const auto& tri : triangles) {
-        AABB box = tri.getbounding();
-        min = Vec3(std::min(min.x, box.min_bound.x), std::min(min.y, box.min_bound.y), std::min(min.z, box.min_bound.z));
-        max = Vec3(std::max(max.x, box.max_bound.x), std::max(max.y, box.max_bound.y), std::max(max.z, box.max_bound.z));
-    }
-    
-    return AABB(min, max);
+  if (triangles.empty())
+    return AABB(Vec3(0, 0, 0), Vec3(0, 0, 0));
+
+  Vec3 min = triangles[0].getbounding().min_bound;
+  Vec3 max = triangles[0].getbounding().max_bound;
+
+  for (const auto &tri : triangles) {
+    AABB box = tri.getbounding();
+    min =
+        Vec3(std::min(min.x, box.min_bound.x), std::min(min.y, box.min_bound.y),
+             std::min(min.z, box.min_bound.z));
+    max =
+        Vec3(std::max(max.x, box.max_bound.x), std::max(max.y, box.max_bound.y),
+             std::max(max.z, box.max_bound.z));
+  }
+
+  return AABB(min, max);
 }
 
-void Mesh::getuv(double& u, double& v, const Vec3& hitPoint) const {
-    u = 0.0;
-    v = 0.0;
+void Mesh::getuv(double &u, double &v, const Vec3 &hitPoint) const {
+  u = 0.0;
+  v = 0.0;
 }
